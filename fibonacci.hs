82ee177b636489fd76675936965e10083c4f8466
hexmyflex <50464528+hexmyflex@users.noreply.github.com>
2020-11-26 23:39:39 +0300

    Create fibonacci.hs

---
+++ fibonacci.hs
@@ -0,0 +1,22 @@
+import Control.Monad.State
+
+fibStep :: State (Integer, Integer) ()
+fibStep = do
+	(a,b) <- get
+	put (b,a+b)
+
+execStateN :: Int -> State s a -> s -> s
+execStateN n m = execState $ replicateM n m
+
+fib :: Int -> Integer
+fib n = fst $ execStateN n fibStep (0, 1)
+
+main :: IO ()
+main = do
+	print $ execState fibStep (0,1)
+	print $ execState fibStep (1,1)
+	print $ execState fibStep (1,2)
+	print $ execStateN 5 fibStep (0, 1)
+	print $ fib 5
+	print $ execStateN 10 fibStep (0, 1)
+	print $ fib 10
