b1fa472f46a09956e9f47e71a99d25d839ad4520
hexmyflex <50464528+hexmyflex@users.noreply.github.com>
2020-11-27 11:37:39 +0300

    Fibonacci numbers task
    Hard task, fibonacci numbers function using less memory and time

--- fibonacci.hs
+++ fibonacci.hs
@@ -1,22 +1,5 @@
-import Control.Monad.State
+fib n | n>=0 = helper 1 0 n
+	  | n<0 = (-1)^(abs n + 1) * fib (-n)
 
-fibStep :: State (Integer, Integer) ()
-fibStep = do
-	(a,b) <- get
-	put (b,a+b)
-
-execStateN :: Int -> State s a -> s -> s
-execStateN n m = execState $ replicateM n m
-
-fib :: Int -> Integer
-fib n = fst $ execStateN n fibStep (0, 1)
-
-main :: IO ()
-main = do
-	print $ execState fibStep (0,1)
-	print $ execState fibStep (1,1)
-	print $ execState fibStep (1,2)
-	print $ execStateN 5 fibStep (0, 1)
-	print $ fib 5
-	print $ execStateN 10 fibStep (0, 1)
-	print $ fib 10
+helper acc1 acc2 0 = acc2
+helper acc1 acc2 n = helper (acc1+acc2) (acc1) (n-1)
