faf375a370d67ef8688850fbdcfbaca7144c9bcf
hexmyflex <50464528+hexmyflex@users.noreply.github.com>
2020-11-27 15:40:59 +0300

    sum and count of digits
    have no idea why this is not working. always getting type error

---
+++ sumofdigits.hs
@@ -0,0 +1,6 @@
+sumcount :: Integer -> (Integer, Integer)
+sumcount x = helper (x,0)
+
+helper :: (Integer, Integer)  -> (Integer, Integer) 
+helper 0 acc2  = 0 acc2 
+helper acc1 acc2  = helper ((1)(1))
